8706ebe51bc2424ba4ba190626b4fde13ae77200
SAR-LOCAL <sar-local@localhost.localdomain>
2025-07-24 08:55:53 +0100

    new test further

--- test/exampleModel.cpp
+++ test/exampleModel.cpp
@@ -1,23 +1,27 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+#include <fstream>
 #include <vector>
-
+#include <array>
 
 using namespace std;
 // Compile with:
-// g++ -std=c++17 -O3 -g -o main main.cpp
+// g++ -std=c++17 -O3 -g -o main exampleModel.cpp
 //
 
+//Fixed width matrix
+template<typename T, unsigned N>
+using  FWMat = vector<array<T,N>>;
 
 //
 // Generate random number
 // using a linear congruential
 // generators
 //
-template<typename INT>
-INT LCG_RNG(INT Seed, INT A, INT C, INT M){
-  return (A*Seed + C)%M;
+uint32_t randqd_uint32(uint32_t rqd_seed){    
+    rqd_seed = (uint32_t) (1664525UL * rqd_seed + 1013904223UL);
+    return rqd_seed;
 };
 
 //
@@ -28,41 +32,35 @@ INT LCG_RNG(INT Seed, INT A, INT C, INT M){
 template<typename Real>
 Real NormalDist(Real x, Real Sig2, Real mu){
   Real PI = Real(3.1415926535897932384626433832795028841971);
-  Real a  = 1.00/sqrt(2*PI*Sig2);
+  Real a  = 1.00/sqrt(2.00*PI*Sig2);
   Real b  = -(x - mu)*(x - mu)/(2.00*Sig2);
   return a*exp(b);
 };
 
-template<typename Real>
-void MetropolisHastings(){
-  //Initialise
 
 
-  //Iterate
-  for(int I=0; I<; I++){
-//    randVecGen(x_dash);  
-//    Real alpha = Func(x_dash);
-
-  };
-};
-
+//
+// Main function
+//
 int main(){
-  int Seed = 3;
-  int M = 2147483648; //2^31
-  int A = 22695477;
-  int C = 1;
-  double Sig2 = 0.03, mu = 0.6;
+  uint32_t rqd_seed = 0UL;
+  double Sig2 = 0.05, mu = 0.5;
+  FWMat<double, 10> epsilon, Sigma;
+  ofstream OutFile;    //The output file
+  OutFile.open("randDistTest.dat"); //The random dist tests
 
+  for(int I=0; I< 500; I++){
+    rqd_seed  = randqd_uint32(rqd_seed);    //sample x
+    double x  = rqd_seed/ double(1L << 32); //Calc   x
+    double z  = NormalDist(x, Sig2, mu);    //Calc   z
+    double Rt = mu + Sig2*z;
 
-  for(int I=0; I< 1000; I++){
-    Seed = LCG_RNG<int> (Seed, A, C, M);
-    double x = double(Seed);
-    double z = NormalDist(x, Sig2, mu);
-    cout << setw(10) << I
-         << setw(10) << x
-         << setw(10) << z << endl;
+    OutFile << setw(16) << I
+            << setw(16) << x
+            << setw(16) << z
+            << setw(16) << Rt << endl;
   }
-
+  OutFile.close();
   return 0;
 };
 
